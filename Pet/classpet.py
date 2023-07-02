4d6fa748ef976dea93174801a8dc4f7f56a63dc3
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-02 20:13:14 +0800

    Setter and getter (class pet)

--- Pet/classpet.py
+++ Pet/classpet.py
@@ -1,4 +1,30 @@
-# Setter and getter
+import pyfiglet
+
+# Setter & getter
+class Pet:
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
+    
 # Display welcome message with colored ASCII art
 # Get pet information from the user
 # Display pet information with colored output
