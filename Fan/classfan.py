c06532400d21a2678fdcd663af5a7ee27bee0112
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-02 08:39:38 +0800

    plotting pseudos for classfan

---
+++ Fan/classfan.py
@@ -0,0 +1,7 @@
+# Define the Fan class
+# Constants for fan speeds
+# Initialize fan attributes
+# FAN .......
+# Print program header using pyfiglet and ANSI escape sequences for color
+# Create an instance of the Fan class
+# Demonstrate the usage of the Fan class methods
