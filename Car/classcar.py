547a1ad0b1bf1239158fc7b26d0860125ddc894f
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-02 10:08:52 +0800

    getter and setter for carclass

--- Car/classcar.py
+++ Car/classcar.py
@@ -1,5 +1,35 @@
 # Define a Car class
-# Initialize the Car object with year_model, make, and speed attributes
-# Method to increase the speed of the car by 5
-# Method to decrease the speed of the car by 5
-# Method to get the current speed of the car
+class Car:
+    # Initialize the Car object with year_model, make, and speed attributes
+    def __init__(self, year_model, make):
+        self.__year_model = year_model  # Assign year_model to the private attribute __year_model
+        self.__make = make  # Assign make to the private attribute __make
+        self.__speed = 0  # Initialize speed to 0
+
+     # Getter method for __year_model attribute
+    def get_year_model(self):
+        return self.__year_model
+
+    # Getter method for __make attribute
+    def get_make(self):
+        return self.__make
+
+    # Getter method for __speed attribute
+    def get_speed(self):
+        return self.__speed
+
+    # Setter method for __year_model attribute
+    def set_year_model(self, year_model):
+        self.__year_model = year_model
+
+    # Setter method for __make attribute
+    def set_make(self, make):
+        self.__make = make
+
+    # Method to increase the speed of the car by 5
+    def accelerate(self):
+        self.__speed += 5
+
+    # Method to decrease the speed of the car by 5
+    def brake(self):
+        self.__speed -= 5
