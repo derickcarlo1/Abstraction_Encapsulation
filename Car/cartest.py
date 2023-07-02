4760b067f5ae10409b49bb73293e87af5d77620f
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-02 10:15:13 +0800

    creating car test and its pseudos

---
+++ Car/cartest.py
@@ -0,0 +1,6 @@
+# Print program header
+# Create a Car object
+# Accelerate the car five times
+# Brake the car five times
+# Modify the car attributes using setters
+# Get the updated car attributes using getters
