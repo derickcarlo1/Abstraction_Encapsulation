dacf501e1f00752ce9a7fc5aa62e2fbdbcd241cd
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-07-02 09:51:12 +0800

    plotting pseudos for class car

---
+++ Car/classcar.py
@@ -0,0 +1,5 @@
+# Define a Car class
+# Initialize the Car object with year_model, make, and speed attributes
+# Method to increase the speed of the car by 5
+# Method to decrease the speed of the car by 5
+# Method to get the current speed of the car
